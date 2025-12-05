7f510e8087c22f59657669e3221f4837af71ab2b
Aman Manwatkar <amanmanwatkar888456@gmail.com>
2025-12-05 23:13:36 +0530

    Add global exception handler for debugging

--- user-service/src/main/java/in/aman/tasks/exception/GlobalExceptionHandler.java
+++ user-service/src/main/java/in/aman/tasks/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package in.aman.tasks.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -7,10 +8,19 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-	@ExceptionHandler(Exception.class)
+    @ExceptionHandler(UserException.class)
+    public ResponseEntity<String> handleUserException(UserException e) {
+        e.printStackTrace();
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body("User Error: " + e.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleAll(Exception e) {
         e.printStackTrace();
-        return ResponseEntity.internalServerError()
-                .body("ERROR: " + e.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Server Error: " + e.getMessage());
     }
 }

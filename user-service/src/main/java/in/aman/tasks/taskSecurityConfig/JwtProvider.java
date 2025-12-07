67cbab57a87797f23335c12d7271d04471a34f1a
Aman Manwatkar <amanmanwatkar888456@gmail.com>
2025-12-07 17:40:15 +0530

    code Updated

--- user-service/src/main/java/in/aman/tasks/taskSecurityConfig/JwtProvider.java
+++ user-service/src/main/java/in/aman/tasks/taskSecurityConfig/JwtProvider.java
@@ -6,15 +6,12 @@ import io.jsonwebtoken.security.Keys;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
-import java.util.Collection;
 import java.util.Date;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Component
 public class JwtProvider {
@@ -24,29 +21,31 @@ public class JwtProvider {
 
     public String generateToken(Authentication auth) {
 
-        String roles = auth.getAuthorities()
-                .stream()
-                .map(GrantedAuthority::getAuthority)
-                .collect(Collectors.joining(","));
+        String email = auth.getName();
+
+        String role = "ROLE_USER";
+
+        if (auth.getPrincipal() instanceof UserDetails userDetails) {
+            role = userDetails.getAuthorities().iterator().next().getAuthority();
+        }
 
         Key key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
 
         String jwt = Jwts.builder()
-                .setSubject(auth.getName())
+                .setSubject(email)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24)) // 24 hrs
-                .claim("email", auth.getName())
-                .claim("authorities", roles)
+                .setExpiration(new Date(System.currentTimeMillis() + 86400000))
+                .claim("email", email)
+                .claim("role", role)
                 .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
 
-        System.out.println("Authorities used for JWT = " + roles);
+        System.out.println("JWT ROLE USED = " + role);
         System.out.println("JWT SECRET USED IN PROVIDER = " + secret);
         System.out.println("Generated Token = " + jwt);
 
         return jwt;
     }
-
 
     public String getEmailFromJwtToken(String jwt) {
 

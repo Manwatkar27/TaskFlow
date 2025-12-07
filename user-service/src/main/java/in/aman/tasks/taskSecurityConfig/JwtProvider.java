6dfd342e664e6c336f9e66a226e5d92064724ef4
Aman Manwatkar <amanmanwatkar888456@gmail.com>
2025-12-07 17:22:20 +0530

    Code Updated

--- user-service/src/main/java/in/aman/tasks/taskSecurityConfig/JwtProvider.java
+++ user-service/src/main/java/in/aman/tasks/taskSecurityConfig/JwtProvider.java
@@ -2,15 +2,19 @@ package in.aman.tasks.taskSecurityConfig;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
 import java.util.Collection;
 import java.util.Date;
-import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Component
 public class JwtProvider {
@@ -20,39 +24,41 @@ public class JwtProvider {
 
     public String generateToken(Authentication auth) {
 
-        Collection<? extends GrantedAuthority> authorities = auth.getAuthorities();
-        String roles = populateAuthorities(authorities);
+        String roles = auth.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
+
+        Key key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
 
         String jwt = Jwts.builder()
+                .setSubject(auth.getName())
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 86400000))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24)) // 24 hrs
                 .claim("email", auth.getName())
                 .claim("authorities", roles)
-                .signWith(SignatureAlgorithm.HS256, secret)
+                .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
 
+        System.out.println("Authorities used for JWT = " + roles);
         System.out.println("JWT SECRET USED IN PROVIDER = " + secret);
         System.out.println("Generated Token = " + jwt);
 
         return jwt;
     }
 
-    private static String populateAuthorities(Collection<? extends GrantedAuthority> authorities) {
-        Set<String> auths = new HashSet<>();
-        for (GrantedAuthority authority : authorities) {
-            auths.add(authority.getAuthority());
-        }
-        return String.join(",", auths);
-    }
 
     public String getEmailFromJwtToken(String jwt) {
 
         jwt = jwt.replace("Bearer ", "");
 
-        return Jwts.parser()
-                .setSigningKey(secret)
+        Key key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
                 .parseClaimsJws(jwt)
                 .getBody()
-                .get("email", String.class);
+                .getSubject();
     }
 }
